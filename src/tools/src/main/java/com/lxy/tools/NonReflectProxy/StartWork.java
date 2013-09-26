ae4e5e871c7a8ebf33786afc09bbff036fa1a8cd
flashmouse <522247671@qq.com>
2013-09-26 22:05:03 +0800

    准备往代理对象里赛代码……

---
+++ src/tools/src/main/java/com/lxy/tools/NonReflectProxy/StartWork.java
@@ -0,0 +1,32 @@
+package com.lxy.tools.NonReflectProxy;
+
+import java.util.List;
+import java.util.Map;
+
+import com.lxy.tools.NonReflectProxy.commons.MethodProxyDefine;
+import com.lxy.tools.NonReflectProxy.configure.AnnotationFinder;
+
+/**
+ * 整个项目的入口
+ * @author lxy
+ *
+ */
+public class StartWork {
+	private static StartWork instance = new StartWork();
+	
+	private StartWork(){
+		
+	}
+	
+	public static StartWork getInstance(){
+		return instance;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public void execute() throws Exception{
+		AnnotationFinder finder = new AnnotationFinder();
+		Map<Class<?>,List<MethodProxyDefine>> proxiedMthods = finder.init();
+		AddProxy addProxy = new AddProxy(proxiedMthods);
+		addProxy.generateCode();
+	}
+}
